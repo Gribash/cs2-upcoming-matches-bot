27332c7d561e48bd51a93e844155f60697d11673
Maksim Gribanov <maksgribanov@MacBook-Pro-Maksim.local>
2025-07-19 22:28:48 +0500

    🚀 Обновление продакшена

--- utils/match_cacher.py
+++ utils/match_cacher.py
@@ -1,13 +1,12 @@
 import asyncio
-import logging
 import sys
 
-from utils.logging_config import setup_logger
+from utils.logging_config import setup_logging
 from utils.cache_writer import write_json_to_cache, MATCHES_CACHE_NAME
 from utils.pandascore import fetch_all_matches
 
 # Настройка логгера
-logger = setup_logger("matches")
+logger = setup_logging("matches")
 
 # Интервал обновления в секундах
 CACHE_INTERVAL_SECONDS = 600  # 10 минут
